81b61571af284e22490af35215e2daa9fc029a7e
Elias Flores <eliasflores13@outlook.com>
2018-10-07 19:34:20 -0400

    Added the register functionality

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import fire from '../config/fire';
+import { Redirect } from 'react-router-dom';
 
 class Register extends Component {
   state = {
     email: '',
     password: '',
+    shouldRedirect: false,
   };
 
   handleChange = (event) => {
@@ -13,7 +16,24 @@ class Register extends Component {
     });
   };
 
+  register = (event) => {
+    event.preventDefault();
+    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+    .then((u)=>{
+      this.setState({ shouldRedirect: true });
+    })
+    .catch((error) => {
+        console.log(error);
+      })
+  };
+
   render() {
+    if (this.state.shouldRedirect) {
+      return (
+        <Redirect to="/" />
+      );
+    }
+
     return (
       <div className="col-md-6">
         <h1>Register page</h1>
@@ -27,7 +47,7 @@ class Register extends Component {
             <label htmlFor="exampleInputPassword1">Password</label>
             <input onChange={this.handleChange} type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" value={this.state.password} />
           </div>
-          <button type="submit" className="btn btn-primary">Register</button>
+          <button onClick={this.register} type="submit" className="btn btn-primary">Register</button>
           <Link to="/login">Have an accoutn? Click to login</Link>
         </form>
       </div>
